54481bb0ab2e6a7e46905785bed9cb330afff883
kh73420 <46505072+kh73420@users.noreply.github.com>
2021-06-30 02:37:35 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var di = document.getElementsByClassName("directions");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+   
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
